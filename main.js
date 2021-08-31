c428fa1f9f41849d0f89cc71171e4fb7976c116f
Jesus Samuel Bedoy <jsambedoy@gmail.com>
2021-08-30 20:49:38 -0700

    redesign

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ window.addEventListener("scroll", function ()
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * -1.5 + "px";
 })
+
+const hello = document.getElementById ("hello");
