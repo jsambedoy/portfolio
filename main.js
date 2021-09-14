92b7d9bc7edfa71032e5ba9449331f31dc141c6d
Jesus Samuel Bedoy <jsambedoy@gmail.com>
2021-09-14 16:03:52 -0700

    ready

--- main.js
+++
@@ -1,9 +0,0 @@
-const parallax = document.getElementById ("parallax");
-
-window.addEventListener("scroll", function ()
-{
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * -1.5 + "px";
-})
-
-const hello = document.getElementById ("hello");
