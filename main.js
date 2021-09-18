6d34f9dae04739fe6ebe4e9757dadf51b88fe9d8
Jesus Samuel Bedoy <jsambedoy@gmail.com>
2021-09-18 10:17:15 -0700

    changed some backgrounds

--- main.js
+++ main.js
@@ -5,11 +5,3 @@ window.addEventListener("scroll", function ()
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * -0.2 + "px";
 })
-
-const parallax_2 = document.getElementById("parallax_2");
-
-window.addEventListener("scroll", function ()
-{
-    let offset = window.pageYOffset;
-    parallax_2.style.backgroundPositionY = offset * -5 + "px";
-})
